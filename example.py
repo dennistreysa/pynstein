84bb48a4b66a8dcdca91ed6a723b1b720399bc29
Dennis <dennistreysa@gmail.com>
2017-07-28 15:35:02 +0200

    [Imp] Renamed 'constraints' to 'conditions' [Add] Added more conditions

--- example.py
+++ example.py
@@ -2,21 +2,21 @@
 
 from pynstein import Pynstein
 
-# constraints:
+# conditions:
 #There are five houses.
-	#The Englishman lives in the red house.
-	#The Spaniard owns the dog.
-	#Coffee is drunk in the green house.
-	#The Ukrainian drinks tea.
+#The Englishman lives in the red house.
+#The Spaniard owns the dog.
+#Coffee is drunk in the green house.
+#The Ukrainian drinks tea.
 #The green house is immediately to the right of the ivory house.
-	#The Old Gold smoker owns snails.
-	#Kools are smoked in the yellow house.
-	#Milk is drunk in the middle house.
-	#The Norwegian lives in the first house.
+#The Old Gold smoker owns snails.
+#Kools are smoked in the yellow house.
+#Milk is drunk in the middle house.
+#The Norwegian lives in the first house.
 #The man who smokes Chesterfields lives in the house next to the man with the fox.
 #Kools are smoked in the house next to the house where the horse is kept.
-	#The Lucky Strike smoker drinks orange juice.
-	#The Japanese smokes Parliaments.
+#The Lucky Strike smoker drinks orange juice.
+#The Japanese smokes Parliaments.
 #The Norwegian lives next to the blue house.
 
 
@@ -24,37 +24,55 @@ from pynstein import Pynstein
 puzzle = Pynstein()
 
 
-# add info-constraints
+# add info-conditions
 
 #The Englishman lives in the red house.
-puzzle.AddConstraint_Info(["Englishman", "red"])
+puzzle.AddCondition_Info(["Englishman", "red"])
 
 #The Spaniard owns the dog.
-puzzle.AddConstraint_Info(["Spaniard", None, "dog"])
+puzzle.AddCondition_Info(["Spaniard", None, "dog"])
 
 #Coffee is drunk in the green house.
-puzzle.AddConstraint_Info([None, "green", None, "coffee"])
+puzzle.AddCondition_Info([None, "green", None, "coffee"])
 
 #The Ukrainian drinks tea.
-puzzle.AddConstraint_Info(["Ukrainian", None, None, "tea"])
+puzzle.AddCondition_Info(["Ukrainian", None, None, "tea"])
 
 #The Old Gold smoker owns snails.
-puzzle.AddConstraint_Info([None, None, "snails", None, "Old Gold"])
+puzzle.AddCondition_Info([None, None, "snails", None, "Old Gold"])
 
 #Kools are smoked in the yellow house.
-puzzle.AddConstraint_Info([None, "yellow", None, None, "Kools"])
+puzzle.AddCondition_Info([None, "yellow", None, None, "Kools"])
 
 #The Lucky Strike smoker drinks orange juice.
-puzzle.AddConstraint_Info([None, None, None, "orange juice", "Lucky Strike"])
+puzzle.AddCondition_Info([None, None, None, "orange juice", "Lucky Strike"])
 
 #The Japanese smokes Parliaments.
-puzzle.AddConstraint_Info(["Japanese", None, None, None, "Parliaments"])
+puzzle.AddCondition_Info(["Japanese", None, None, None, "Parliaments"])
 
 
-# add position-constraints
+# add position-conditions
 
 #Milk is drunk in the middle house.
-puzzle.AddConstraint_Position([None, None, None, "milk"], 2)
+puzzle.AddCondition_Position([None, None, None, "milk"], 2)
 
 #The Norwegian lives in the first house.
-puzzle.AddConstraint_Position(["Norwegian"], 0)
+puzzle.AddCondition_Position(["Norwegian"], 0)
+
+
+# add nextTo conditions
+
+#The man who smokes Chesterfields lives in the house next to the man with the fox.
+puzzle.AddCondition_NextTo([None, None, None, None, "Chesterfields"], [None, None, "fox"])
+
+#Kools are smoked in the house next to the house where the horse is kept.
+puzzle.AddCondition_NextTo([None, None, None, None, "Kools"], [None, None, "horse"])
+
+#The Norwegian lives next to the blue house.
+puzzle.AddCondition_NextTo(["Norwegian"], [None, "blue"])
+
+
+# add rightOf conditions
+
+#The green house is immediately to the right of the ivory house.
+puzzle.AddCondition_RightOf([None, "green"], [None, "ivory"])
--- pynstein.py
+++ pynstein.py
@@ -8,40 +8,81 @@ class Pynstein(object):
 	def _init(self):
 		self._rows = None
 		self._cols = None
-		self._constraints = {
+		self._conditions = {
 			"info" : [],
-			"position": {}
+			"position": {},
+			"next_to": [],
+			"left_of": []
 		}
 
-	def _checkConstraint(self, constraint):
-		assert isinstance(constraint, list), "Constraint has to be listtype!"
-		assert len(constraint) > 0, "Could not find any data"
-		assert len([c for c in constraint if c is not None]) > 0, "Could not find any non-none data!"
+	def _checkColumn(self, column):
+		assert isinstance(column, list), "Column has to be listtype!"
+		assert len(column) > 0, "Could not find any data"
+		assert len([c for c in column if c is not None]) > 0, "Could not find any non-none data!"
 
 	def _checkPosition(self, position):
 		assert isinstance(position, int), "Position has to be int!"
 		assert position >= 0, "Position has to be int!"
 
-	def AddConstraint_Info(self, constraint):
-		""" Add an info constraint
+	def _getPosition(self, column):
+		""" Tries to find the position of a given column in the current puzzle-matrix
+		Returns index >= 0 if found, else None
+		"""
+		pass
+
+	def _checkConditions(self):
+		""" Checks all conditions
+		Returns True, if all conditions are successfully met
+		"""
+		pass
+
+	def AddCondition_Info(self, column, exclude=False):
+		""" Add an info condition
 		Info contraints consist of just one column
 		e.g. "The Englishman lives in the red house"
 		"""
-		self._checkConstraint(constraint)
-		self._constraints["info"].append(constraint)
+		self._checkColumn(column)
+		self._conditions["info"].append({"column": column, "exclude": exclude})
 
-	def AddConstraint_Position(self, constraint, position):
-		""" Add a position constraint
-		Position contraints are like info constraints, but have a fixed position
+	def AddCondition_Position(self, column, position, exclude=False):
+		""" Add a position condition
+		Position conditions are like info conditions, but have a fixed position
 		e.g. "The Norwegian lives in the first house"
 		"""
-		self._checkConstraint(constraint)
+		self._checkColumn(column)
 		self._checkPosition(position)
-		if position not in self._constraints["position"]:
-			self._constraints["position"][position] = []
-		self._constraints["position"][position].append(constraint)
+		if position not in self._conditions["position"]:
+			self._conditions["position"][position] = []
+		self._conditions["position"][position].append({"column": column, "exclude": exclude})
+
+	def AddCondition_NextTo(self, column1, column2, exclude=False):
+		""" Add a nextTo condition
+		NextTo conditions mean that to columns are next to each other, regardles of the order
+		e.g. "The man who smokes Chesterfields lives in the house next to the man with the fox"
+		"""
+		self._checkColumn(column1)
+		self._checkColumn(column2)
+		self._conditions["next_to"].append({"column1": column1, "column2": column2})
+
+	def AddCondition_LeftOf(self, leftColumn, rightColumn, exclude=False):
+		""" Add a leftOf condition
+		LeftOf conditions are like nextTo condions, but with a difinite order of the columns
+		e.g. "The green house is immediately to the right/left of the ivory house"
+		"""
+		self._checkColumn(leftColumn)
+		self._checkColumn(rightColumn)
+		self._conditions["left_of"].append({"leftColumn": leftColumn, "rightColumn": rightColumn})
+
+	def AddCondition_RightOf(self, rightColumn, leftColumn, exclude=False):
+		""" Add a rightOf condition
+		This is the same as the leftOf condition, just with swapped columns
+		"""
+		self.AddCondition_LeftOf(leftColumn, rightColumn, exclude)
 
 	def Prepare(self):
+		pass
+
+	def Solve(self):
 		pass
 
 	def StartOver(self):
@@ -54,3 +95,7 @@ class Pynstein(object):
 	@property
 	def Cols(self):
 		return self._cols
+
+	@property
+	def Items(self):
+		return []
